4bb184b67402f6ea6861d90ee082e30fa82d6b7d
Aya691 <dahia7358@icloud.com>
2024-10-22 09:23:00 +0200

    architecture du prgramme

--- include/3W.h
+++ include/3W.h
@@ -1,2 +1,31 @@
-#pragma once;
+#pragma once
 
+#define CONFIGURATION 0
+#define STANDARD 1
+#define ECONOMIQUE 2
+#define MAINTENANCE 3
+
+const int buttonPin1 = 2;
+const int buttonPin2 = 3;
+const int ledPinR = 9;
+const int ledPinG = 10;
+const int ledPinB = 11;
+
+int buttonState1 = 0;
+int buttonState2 = 0;
+unsigned long lastButtonPress1 = 0;
+unsigned long lastButtonPress2 = 0;
+int currentMode = STANDARD;
+int sdCardInserted = 0;
+int dataAvailable = 0;
+
+typedef struct {
+    int pressionAtmo;
+    int temperatureAir;
+    int hygrometrie;
+} Capteurs;
+
+void modeConfiguration();
+void modeStandard();
+void modeEconomique();
+void modeMaintenance();
