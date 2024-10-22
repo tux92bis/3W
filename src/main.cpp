c5867ecb8960376049b97cd0ac39847c2984071a
GeorgesZam <154804622+GeorgesZam@users.noreply.github.com>
2024-10-22 10:54:09 +0200

    Update main.cpp
    update des fonctions getData et saveData avec créeation de la fonction get time

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,41 @@
 #include <Arduino.h>
 #include "3W.h"
 
+
 // put function declarations here:
-int myFunction(int, int);
-int get_data() { 
+
+int get_data() {
+  int ldrValue = analogRead(ldrPin); // Lecture de la photorésistance 
+  float temperature = dht.readTemperature(); // Lecture de la température
+  float humidity = dht.readHumidity(); //Lecture de l'humidité
 }
-save_data_csv{}
+String get_time() {
+    DateTime now = rtc.now();  // Obtenir l'heure actuelle depuis le RTC
+    String time = String(now.year()) + "-" + String(now.month()) + "-" + String(now.day()) + " " +
+                  String(now.hour()) + ":" + String(now.minute()) + ":" + String(now.second());
+    return time;
+}
+
+void save_data_csv(float temperature, float humidity, int luminosity, String time) {
+    File dataFile = SD.open("data.csv", FILE_WRITE);
+    if (dataFile) {
+        dataFile.print(time);            // Sauvegarder le temps
+        dataFile.print(",");
+        dataFile.print(temperature);     // Sauvegarder la température
+        dataFile.print(",");
+        dataFile.print(humidity);        // Sauvegarder l'humidité
+        dataFile.print(",");
+        dataFile.println(luminosity);    // Sauvegarder la luminosité
+        dataFile.close();
+    } else {
+        Serial.println("Erreur d'ouverture du fichier");
+    }
+}
 
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  
 }
 
 void loop() {
