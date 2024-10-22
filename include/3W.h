7dbe5ff8e6c13474909c8af1c3c75e19345e86d2
Micka13 <150048395+Micka1310@users.noreply.github.com>
2024-10-22 11:50:29 +0200

    Ajout de Macros

--- include/3W.h
+++ include/3W.h
@@ -1,9 +1,17 @@
-#pragma once
+#pragma once   // Pour éviter d'appeler plusieurs fichier en-tête
 
-#define CONFIGURATION 
-#define STANDARD 
-#define ECONOMIQUE 
-#define MAINTENANCE 
+/*-----Définition des macros-----*/
+#define BOUTON_ROUGE 2   // Port du bouton rouge
+#define BOUTON_VERT 3   // Port du bouton vert
+#define CAPTEUR 4   // Port du capteur
+#define PHOTORESISTANCE A0   // Port de la photorésistance
+
+// Utiliser pour basculer entra chaque mode
+#define CONFIGURATION 0   
+#define STANDARD 1
+#define ECONOMIQUE 2
+#define MAINTENANCE 3
+
 
 typedef struct {
     int pressionAtmo;
