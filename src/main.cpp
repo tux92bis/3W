874fd774a7af3c823c7c58080d0f1e5ecfef1367
Gabriel Nunès <nunes.gabriel@outlook.fr>
2024-10-22 14:04:37 +0200

    Ajouter des bibliotheques propriétaire + traduction code + restructuration main

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,11 @@
 #include "3W.h"   // Inclus notre fichier en-tête
+#include <Arduino.h>
 
 /*-----code des fonctions-----*/
-int get_data() {
-  int ldrValue = analogRead(ldrPin); // Lecture de la photorésistance 
-  float temperature = dht.readTemperature(); // Lecture de la température
-  float humidity = dht.readHumidity(); //Lecture de l'humidité
-}
-String get_time() {
-  DateTime now = rtc.now();  // Obtenir l'heure actuelle depuis le RTC
-  String time = String(now.year()) + "-" + String(now.month()) + "-" + String(now.day()) + " " + String(now.hour()) + ":" + String(now.minute()) + ":" + String(now.second());
-  return time;
-}
-
-void save_data_csv(float temperature, float humidity, int luminosity, String time) {
-    File dataFile = SD.open("data.csv", FILE_WRITE);
-    if (dataFile) {
-        dataFile.print(time);            // Sauvegarder le temps
-        dataFile.print(",");
-        dataFile.print(temperature);     // Sauvegarder la température
-        dataFile.print(",");
-        dataFile.print(humidity);        // Sauvegarder l'humidité
-        dataFile.print(",");
-        dataFile.println(luminosity);    // Sauvegarder la luminosité
-        dataFile.close();
-    } else {
-        Serial.println("Erreur d'ouverture du fichier");
-    }
-}
 
 // structure des données pour le capteur
-typedef struct {
-    int pressionAtmo;
+typedef struct 
+{
     int temperatureAir;
     int hygrometrie;
     int lumiere;
@@ -45,8 +20,9 @@ void setup() {
   pinMode(BOUTON_VERT, INPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+  /*A completer : fonction qui appel la fonction de mode approprier en fonction de l'état initial et des fonctions d'interruptions.*/ 
 }
 
 // put function definitions here:
