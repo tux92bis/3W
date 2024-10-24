55c28b41f5f80dd07e0b4ef1ca494718494f4e93
Micka13 <150048395+Micka1310@users.noreply.github.com>
2024-10-24 12:08:32 +0200

    couleur led en mode configuration

--- src/configuration.cpp
+++ src/configuration.cpp
@@ -12,3 +12,15 @@ configue(){
     }
 }
 
+/* 
+Macros à ajouter dans le fichier "3W" :
+LED_ROUGE = broche 7
+LED_VERT = broche 6
+LED_BLEU = broche 5
+*/
+void couleurLedConfiguration() {
+  // Pour afficher une couleur jaune à la led
+  analogWrite(LED_ROUGE, 255);   
+  analogWrite(LED_VERT, 255);
+  analogWrite(LED_BLEU, 0);
+}
