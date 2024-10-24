6b2154e32d25f9fd3379b13e5137648688f4e139
Micka13 <150048395+Micka1310@users.noreply.github.com>
2024-10-24 12:06:00 +0200

    couleud de la led en mode maintenance

--- src/maintenance.cpp
+++ src/maintenance.cpp
@@ -1,1 +1,14 @@
 #include "3W.h"
+
+/* 
+Macros à ajouter dans le fichier "3W" :
+LED_ROUGE = broche 7
+LED_VERT = broche 6
+LED_BLEU = broche 5
+*/
+void couleurLedStandard() {
+  // Pour afficher une couleur orange à la led
+  analogWrite(LED_ROUGE, 255);   
+  analogWrite(LED_VERT, 165);
+  analogWrite(LED_BLEU, 0);
+}
