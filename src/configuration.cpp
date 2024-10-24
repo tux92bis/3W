de185e0d7e66a214db42326b74fa3cd49a394445
Aya691 <dahia7358@icloud.com>
2024-10-24 09:38:50 +0200

    mode configuration

--- src/configuration.cpp
+++ src/configuration.cpp
@@ -1,26 +1,5 @@
+#include <Arduino.h>
 #include "3W.h"
-configue(){
-  int temps de démarage = millis();
-  int temps actuel = millis();
-  
-  while (temps de démarage - temp actuel ≤ 30 s) { 
-  
-    // le mode config ici
-  
-    temps actuel = milllis();
-  
-    }
-}
 
-/* 
-Macros à ajouter dans le fichier "3W" :
-LED_ROUGE = broche 7
-LED_VERT = broche 6
-LED_BLEU = broche 5
-*/
-void couleurLedConfiguration() {
-  // Pour afficher une couleur jaune à la led
-  analogWrite(LED_ROUGE, 255);   
-  analogWrite(LED_VERT, 255);
-  analogWrite(LED_BLEU, 0);
-}
+
+
