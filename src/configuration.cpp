3c35c357f38ed41fe52457f6dc97de37a3d68a95
GeorgesZam <154804622+GeorgesZam@users.noreply.github.com>
2024-10-24 11:18:04 +0200

    mesure de 30mn dans la fonction ModeConfig()

--- src/configuration.cpp
+++ src/configuration.cpp
@@ -1,4 +1,14 @@
 #include "3W.h"
-
-
+configue(){
+  int temps de démarage = millis();
+  int temps actuel = millis();
+  
+  while (temps de démarage - temp actuel ≤ 30 s) { 
+  
+    // le mode config ici
+  
+    temps actuel = milllis();
+  
+    }
+}
 
