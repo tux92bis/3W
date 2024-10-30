1384ccb842aa00ecf72afa916ee514c65cb43fdf
Micka13 <150048395+Micka1310@users.noreply.github.com>
2024-10-30 21:41:17 +0100

    Déclaration de macros, de variables et fonctions

--- include/3W.h
+++ include/3W.h
@@ -1,18 +1,71 @@
-#pragma once   // Pour éviter d'appeler plusieurs fichier en-tête
+/*-----Fichier Header-----*/
 
-/*-----Définition des macros-----*/
+#pragma once   // Pour éviter d'appeler plusieurs fichier en-tête identique
+
+#include <Arduino.h>
+
+
+//Définition des macros
 #define BOUTON_ROUGE 2   // Port du bouton rouge
 #define BOUTON_VERT 3   // Port du bouton vert
-#define CAPTEUR 4   // Port du capteur
-#define PHOTORESISTANCE A0   // Port de la photorésistance
 
-// Utiliser pour basculer entra chaque mode
+#define LED_ROUGE 8   // port de la couleur rouge pour la LED
+#define LED_BLEU 10    // port de la couleur bleu pour la LED
+#define LED_VERT 9    // port de la couleur vert pour la LED
+
+
+//Utiliser pour basculer entre chaque mode
 #define CONFIGURATION 0   
 #define STANDARD 1
 #define ECONOMIQUE 2
 #define MAINTENANCE 3
 
-void modeConfiguration();
+
+//-----Déclaration des variables globales------
+//Pour le fichier "fonction_main.cpp"
+extern int modeActuel;
+extern int modePrecedent;
+extern volatile bool boutonAppuyeVert;
+extern volatile bool boutonAppuyeRouge;
+extern unsigned long dureeAppui;
+
+//Pour le fichier "configuration.cpp"
+extern int logInterval;
+extern int fileMaxSize;
+extern int timeout;
+extern int lumin;
+extern int luminLow;
+extern int luminHigh;
+extern unsigned long lastActivityTime;
+
+
+//Déclaration des fonctions du fichier "fonction_main.cpp"
+void boutonRougePresser();
+void boutonVertPresser();
+//void clignoterLED();
+void couleurLedMaintenance();
+void couleurLedStandard();
+void couleurLedConfiguration();
+void couleurLedEconomie();
+
+
+//Déclaration des fonctions du fichier "standart.cpp"
 void modeStandard();
-void modeEconomique();
+int obtenir_donnees();
+String obtenir_temps();
+void sauvegarder_donnees_csv();
+void couleurLedStandard();
+
+
+//Déclaration des fonctions du fichier "configuration.cpp"
+void modeConfiguration();
+void saveToEEPROM();
+void resetToDefaults();
+
+
+//Déclaration des fonctions du fichier "maintenance.cpp"
 void modeMaintenance();
+
+
+//Déclaration des fonctions du fichier "economique.cpp"
+void modeEconomique();
