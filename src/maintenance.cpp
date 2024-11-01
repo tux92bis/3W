3652a599eed618e16e5be168552d11ef8928bb71
Rayan <81958556+YuGxKuS4N@users.noreply.github.com>
2024-11-01 17:56:18 +0100

    Update maintenance.cpp

--- src/maintenance.cpp
+++ src/maintenance.cpp
@@ -1,6 +1,7 @@
 #include "3W.h" // Inclusion du fichier en-tête
 #include <RTClib.h>
 #include <LiquidCrystal.h>
+#include <DHT.h>
 
 extern int modeCourant;      // Mode courant défini dans main.cpp
 extern Capteurs capteurs;    // Structure des capteurs
