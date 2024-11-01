6b7a1ebd112439590771a3dd18c067958e9f1c50
Rayan <81958556+YuGxKuS4N@users.noreply.github.com>
2024-11-01 17:47:06 +0100

    Update maintenance.cpp

--- src/maintenance.cpp
+++ src/maintenance.cpp
@@ -4,6 +4,21 @@ extern int modeCourant;      // Mode courant défini dans main.cpp
 extern Capteurs capteurs;    // Structure des capteurs
 extern bool ecritureSD;      // Variable pour gérer l'écriture sur la SD
 
+Capteurs get_data() {
+    Capteurs data;
+    data.lumiere = analogRead(PHOTORESISTANCE);
+    data.temperatureAir = dht.readTemperature();
+    data.hygrometrie = dht.readHumidity();
+    return data;
+}
+
+String get_time() {
+    DateTime now = rtc.now();
+    String time = String(now.year()) + "-" + String(now.month()) + "-" + String(now.day()) + " " +
+                  String(now.hour()) + ":" + String(now.minute()) + ":" + String(now.second());
+    return time;
+}
+
 // Fonction pour passer en mode maintenance
 void modeMaintenance() {
     lcd.clear();
